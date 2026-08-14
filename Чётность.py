11fbd70c7dbf6442b2a8fc2054d0c2ebc4ff43da
AnsyTuO <AnsiaEvmo@ya.ru>
2026-08-14 13:32:18 +0300

    Add files via upload

---
+++ Чётность.py
@@ -0,0 +1,6 @@
+print("Чётное или нечётное!")
+number=float(input("Ваше число: "))
+if number %2==0:
+    print(f"Число {number} чётное.")
+elif number %2!=0:
+    print(f"Число {number} нечётное.")
